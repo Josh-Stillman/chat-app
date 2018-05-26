68e1b575abb5137e20193bc82970ae2839d5a0cd
Josh-Stillman <joshstillman@gmail.com>
2018-05-26 12:29:27 -0400

    change unnecessary let declarations to consts

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import {Form, Input} from 'semantic-ui-react'
-let moment = require('moment');
+const moment = require('moment');
 
 
 class InputForm extends Component {
